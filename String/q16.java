f4465e5ae5ac843037ca58843c87be907c84d0ea
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 16:41:39 +0300

    16

--- String/q16.java
+++ String/q16.java
@@ -1,3 +1,23 @@
 //16.Convert the first letter of each word to uppercase.
 
 
+public static void main(String[] args) {
+    String s = "hi guys how are you" ;
+   String arr[] = s.split(" ");
+           System.out.println(" Before converting the first letter of each word to uppercase. : "+s);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (char) (arr[i].charAt(0) ^ 32) + arr[i].substring(1);
+        }
+        
+          s = "";
+        for (int i = 0; i < arr.length; i++) {
+            s += arr[i];
+            if (i == arr.length - 1) {
+                break;
+            }
+            s += " ";
+        }
+        System.out.println(" After converting the first letter of each word to uppercase. : "+s);
+        
+}
