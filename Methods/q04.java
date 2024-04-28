1a253a435c06e94fce06918ae48e9c321b3d3c5a
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-29 00:54:23 +0300

    4

--- Methods/q04.java
+++ Methods/q04.java
@@ -1,6 +1,7 @@
 // 4. Create a method to calculate the factorial of a given number.
 
 public static long factorial(int n) {
+        
         long fact = 1;
         for (int i = 1; i <= n; i++) {
             fact *= i;
