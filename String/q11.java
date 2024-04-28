29f1619a3c5cd979789b0ae90e1f9dc2e69c72a5
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 16:07:30 +0300

    11

---
+++ String/q11.java
@@ -0,0 +1,8 @@
+//11.Rotate left by one character.
+
+public static void main(String[] args) {
+    String s = "layan";
+     s =s.substring(1) + s.charAt(0) ;
+     System.out.println("after rotate left : "+ s );
+    
+}
