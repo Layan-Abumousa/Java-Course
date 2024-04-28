87a718786b1c1439a5df699b7b3eb103b07f849c
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 14:51:56 +0300

    Q1

---
+++ String/Q1.java
@@ -0,0 +1,15 @@
+//1.Find the frequency of a character in a string.
+
+public static void main(String[] args) {
+    String s = "hello world";
+    char ch = 'l';
+    int c = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == ch) {
+            c++;
+        }
+    }
+
+    System.out.println("The frequency of '" + ch + "' in \"" + s + "\" is: " + c);
+}
