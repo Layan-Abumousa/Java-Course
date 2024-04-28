44fad379a8946fe7a923e7b5c5d6cc466bfd1051
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 17:05:14 +0300

    24

--- String/q24.java
+++ String/q24.java
@@ -2,9 +2,9 @@
 
 public static void main(String[] args) {
       
-         String s = "sjsjj";
+       String s = "sjsjj";
+       int odd = 0;
        int freq[] = freq(s.toCharArray());
-        int odd = 0;
         for (int i = 0; i < freq.length; i++) {
             if (freq[i] % 2 != 0) {
                 odd++;
