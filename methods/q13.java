ee2757c02316fbe97e2e6c2e69bf47d1ecbec56b
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-29 00:28:12 +0300

    13

--- methods/q13.java
+++ methods/q13.java
@@ -1,5 +1,6 @@
 // 13. Implement a method to convert temperature from Celsius to Fahrenheit.
 
  public static double celsiusToFahrenheit(double celsius) {
+  
         return (9.0 / 5.0) * celsius + 32; 
     }
