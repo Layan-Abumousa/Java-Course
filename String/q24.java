009a9f6b3b4086480a90eb364b50c718f5f7a1f1
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 17:04:45 +0300

    24

--- String/q24.java
+++ String/q24.java
@@ -1,1 +1,19 @@
 // 24.Check if it's possible for the string to be a palindrome.
+
+public static void main(String[] args) {
+      
+         String s = "sjsjj";
+       int freq[] = freq(s.toCharArray());
+        int odd = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] % 2 != 0) {
+                odd++;
+            }
+        }
+        if (odd > 1) {
+            System.out.println("won't be pal");
+        }
+        else
+            System.out.println("May be Pal");
+
+}
