79505e2f9ca50dcb7ffdaa172fa0d2bdeedc3a46
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-29 00:18:04 +0300

    9

---
+++ methods/q09.java
@@ -0,0 +1,6 @@
+// 9. Implement a method to calculate the area of a circle given its radius.
+
+ public static double Area(double radius) {
+        
+        return Math.PI * radius * radius; 
+    }
