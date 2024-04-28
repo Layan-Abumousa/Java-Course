2b96fb96d4652b80319f4535676107839b71791c
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-29 00:00:52 +0300

    2

---
+++ methods/q2.java
@@ -0,0 +1,7 @@
+// 2. Java method to print array elements.
+
+public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
