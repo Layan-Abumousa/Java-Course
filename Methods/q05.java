9fbabe344f532909e2e213cbdc09044a253cd2d1
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-29 00:53:32 +0300

    Add files via upload

---
+++ Methods/q05.java
@@ -0,0 +1,15 @@
+//5. Implement a method to check if a number is prime or not.
+
+
+public static boolean prime(int n) {
+        int c = 0; 
+        for (int i = 1; i <= n; i++) {
+            if (n%i==0) {
+                c++;
+            }
+       }
+        if (c==2) {
+            return true;
+       }
+        return false;
+    }
