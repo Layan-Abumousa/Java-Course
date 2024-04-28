dee1f3b4025a05a7a1d1f73ea034317c02eb79d3
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-04-28 16:38:07 +0300

    16

--- String/q17.java
+++ String/q17.java
@@ -1,1 +1,26 @@
-// 16.Convert the first letter of each word to uppercase.
+//17.Replace one word with another.
+
+
+public static void main(String[] args) {
+    String s = "hi guys how are you" ;
+    String s1 = "hi" , s2 ="holaa";
+    System.out.println("Before replacing : "+s);
+
+    String arr[] = s.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(s1)) {
+                arr[i] = s2;
+            }
+        }
+        
+          s = "";
+        for (int i = 0; i < arr.length; i++) {
+            s += arr[i];
+            if (i == arr.length - 1) {
+                break;
+            }
+            s += " ";
+        }
+        System.out.println("After replacing : "+s);
+        
+}
