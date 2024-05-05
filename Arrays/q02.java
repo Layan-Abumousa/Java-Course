e7ba05e3e9bd606b248b018612a6a12029dfb558
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-05-05 23:53:47 +0300

    2

---
+++ Arrays/q02.java
@@ -0,0 +1,8 @@
+// 2.Print the array in reverse. 
+
+public static void printInReverse(int[]arr){
+        for (int i = arr.length-1; i >=0; i--) {
+                System.out.print(arr[i]+"  ");
+            }
+        
+    }
