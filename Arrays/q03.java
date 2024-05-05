6776f967b89c6ae19edf6b398730a746bc041ef6
Layan AbuMousa <168235630+Layan-Abumousa@users.noreply.github.com>
2024-05-05 23:55:40 +0300

    3

---
+++ Arrays/q03.java
@@ -0,0 +1,9 @@
+// 3.Find the sum of array elements. 
+
+ public static int sumOfElements(int[]arr){
+       int sum = 0; 
+        for (int i = 0; i <arr.length; i++) {
+            sum +=arr[i];
+        }
+        return sum;
+    }
